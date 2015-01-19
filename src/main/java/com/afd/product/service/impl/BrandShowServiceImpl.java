7db6d8a7c62dbeb5581136694818ebab0f7631a1
wangxiaopeng <wangxiaopeng@yiwang.com>
2015-01-19 10:29:13 +0800

    特卖服务

---
+++ src/main/java/com/afd/product/service/impl/BrandShowServiceImpl.java
@@ -0,0 +1,36 @@
+package com.afd.product.service.impl;
+
+import java.util.List;
+
+import com.afd.model.product.BrandShow;
+import com.afd.model.product.BrandShowDetail;
+import com.afd.service.product.IBrandShowService;
+
+public class BrandShowServiceImpl implements IBrandShowService{
+
+	@Override
+	public BrandShow getBrandShowById(Long brandShowId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BrandShowDetail getBrandShowDetailById(Long brandShowDetailId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BrandShow> getBrandShowByIds(List<Long> brandShowIds) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BrandShowDetail> getBrandShowDetailsByIds(
+			List<Long> brandShowDetailIds) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
